1defd7636366f5774c54cae0092d49f2ca5b1152
Padula Guruge <padula@ascentic.se>
2025-04-02 19:50:37 +0530

    btn change

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -24,7 +24,7 @@ export default function Home() {
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <form onSubmit={submitForm} className='flex flex-col gap-4'>
         <input type="text" placeholder="Enter your name" value={name} onChange={(e)=> setName(e.target.value)} className='border p-2 rounded'/>
-        <button type="submit" className='bg-blue-500 rounded p-2'>Submit</button>
+        <button type="submit" className='bg-blue-500 rounded p-2'>Click Me</button>
       </form>
       {
         res && (
